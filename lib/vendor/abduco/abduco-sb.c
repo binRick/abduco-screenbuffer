7b01e3241765ea9e91775b33135a9fd840fb7934
root <root@f36.vpnservice.company>
2022-02-20 05:10:10 +0000

    m

--- lib/vendor/abduco/abduco-sb.c
+++ lib/vendor/abduco/abduco-sb.c
@@ -63,14 +63,8 @@
 
 #define countof(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-enum PacketType {
-	MSG_CONTENT = 0,
-	MSG_ATTACH  = 1,
-	MSG_DETACH  = 2,
-	MSG_RESIZE  = 3,
-	MSG_EXIT    = 4,
-	MSG_PID     = 5,
-};
+#include "types.h"
+
 
 typedef struct {
 	uint32_t type;
---
+++ lib/vendor/abduco/types.h
@@ -0,0 +1,16 @@
+
+enum PacketType {
+  MSG_CONTENT = 0,
+  MSG_ATTACH  = 1,
+  MSG_DETACH  = 2,
+  MSG_RESIZE  = 3,
+  MSG_EXIT    = 4,
+  MSG_PID     = 5,
+};
+
+
+
+
+
+
+
